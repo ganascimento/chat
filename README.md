1c7277d9ad414f5ee61c009b55b709dcefb04859
Guilherme Araujo <guih.a8@gmail.com>
2020-07-15 08:03:49 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Chat
+
+#### This project is a simple chat
+
+<p align="center">
+  <img src="http://www.gadev.com.br/chat/chat_example.gif" />
+</p>
+
+It contains some functions, like:
+  * Login and register
+  * Send invites
+  * Acept or refused invites
+  * Expand photo
+  * Check if the friend is online
+
+It was developed using the following technologies:
+
+Front-end:
+  * ReactJS
+  * TypeScript
+  * Redux
+  * Redux-Saga
+  * React Hooks
+  * Styled-Components
+  
+Back-end:
+  * C#
+  * .NET Core 3.1
+  * SQL Server
+  * SignalR
+  * Dapper
+  * Identity
+  * JSON Web Token (JWT)
+  * Elmah
---
+++ chatweb/build/precache-manifest.f007df9f0dd6e3086999215f1fa35754.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b30b69a06b98ed6391dab2e07d55c1",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "6754583b4078ab400035",
+    "url": "/chat/static/css/2.05dbf286.chunk.css"
+  },
+  {
+    "revision": "f426517244f6dbbb38db",
+    "url": "/chat/static/css/main.a1cf698f.chunk.css"
+  },
+  {
+    "revision": "6754583b4078ab400035",
+    "url": "/chat/static/js/2.36cc0660.chunk.js"
+  },
+  {
+    "revision": "872d4ad223c39fec1068d862c0b329ff",
+    "url": "/chat/static/js/2.36cc0660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f426517244f6dbbb38db",
+    "url": "/chat/static/js/main.1b372f7d.chunk.js"
+  },
+  {
+    "revision": "be42e718218adb486349",
+    "url": "/chat/static/js/runtime-main.448e1c57.js"
+  },
+  {
+    "revision": "75365efdb746b3e14b37b4ab21fa6ed3",
+    "url": "/chat/static/media/Quicksand-Medium.75365efd.ttf"
+  }
+]);
